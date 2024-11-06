849482c7c5377bcdb1855f1026e8c7953c2f13a1
SharipovIskandar <koklok444@gmail.com>
2024-11-06 18:48:59 +0500

    asdf

--- app/MoonShine/Resources/ImagesResource.php
+++ app/MoonShine/Resources/ImagesResource.php
@@ -32,7 +32,7 @@ class ImagesResource extends ModelResource
     {
         return [
             Block::make([
-               Image::make("image_path")
+               Image::make("Image", "image_path")
             ]),
         ];
     }

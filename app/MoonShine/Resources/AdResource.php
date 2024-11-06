e74b1feee425c47f49998a685754620ce7fc94ae
SharipovIskandar <koklok444@gmail.com>
2024-11-06 18:13:25 +0500

    sdf

--- app/MoonShine/Resources/AdResource.php
+++ app/MoonShine/Resources/AdResource.php
@@ -5,17 +5,17 @@ declare(strict_types=1);
 namespace App\MoonShine\Resources;
 
 use App\Enums\Gender;
-use App\Moonshine\MoonshineImage;
-use Faker\Core\Number;
-use Faker\Provider\Text;
+use Moonshine\Fields\Image as MoonshineImage;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Ad;
 
 use Illuminate\Support\Str;
 use MoonShine\Fields\Enum;
 use MoonShine\Fields\Image;
+use MoonShine\Fields\Number;
 use MoonShine\Fields\Relationships\BelongsTo;
 use MoonShine\Fields\Relationships\HasMany;
+use MoonShine\Fields\Text;
 use MoonShine\Fields\Textarea;
 use MoonShine\Resources\ModelResource;
 use MoonShine\Decorations\Block;
@@ -44,30 +44,28 @@ class AdResource extends ModelResource
                     ->disk('public')
                     ->dir('images')
                     ->allowedExtensions(['jpg', 'png', 'jpeg'])
-                    ->afterSave(function($imageField, $item) {
+                    ->afterSave(function ($imageField, $item) {
                         $uniqueName = Str::uuid() . '.' . $imageField->getExtension();
                         $path = 'images/' . $uniqueName;
 
-                        // Store the image at the desired path
                         $imageField->storeAs('public/images', $uniqueName);
 
-                        // Save the path in the images table
                         Image::create([
                             'image_path' => $path,
                             'ad_id' => $item->id,
                         ]);
                     }),
-                \MoonShine\Fields\Text::make("title"),
-                \MoonShine\Fields\Text::make("description")->hideOnIndex(),
+                Text::make("title"),
+                Text::make("description")->hideOnIndex(),
                 Textarea::make("address"),
-                \MoonShine\Fields\Number::make("rooms")->sortable(),
-                \MoonShine\Fields\Number::make("price")->sortable(),
+                Number::make("rooms")->sortable(),
+                Number::make("price")->sortable(),
                 Enum::make("gender")->attach(Gender::class)->sortable(),
-                BelongsTo::make(label: 'branch',relationName: 'branch' , resource: new BranchResource()),
+                BelongsTo::make(label: 'branch', relationName: 'branch', resource: new BranchResource()),
                 BelongsTo::make(label: 'status', resource: new StatusResource()),
                 BelongsTo::make(label: 'user', resource: new UserResource()),
-                BelongsTo::make(label: 'Mualif',relationName: 'owner' ,resource: new  UserResource()),
-                HasMany::make("images",relationName: "images" ,resource: new ImagesResource())->onlyLink(),
+                BelongsTo::make(label: 'Mualif', relationName: 'owner', resource: new  UserResource()),
+                HasMany::make("images", relationName: "images", resource: new ImagesResource())->onlyLink(),
 
 
             ]),

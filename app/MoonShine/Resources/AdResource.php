2fd2e750b9643c4c1238ad18ec498e80197de6cd
SharipovIskandar <koklok444@gmail.com>
2024-11-06 18:24:02 +0500

    qwe

--- app/MoonShine/Resources/AdResource.php
+++ app/MoonShine/Resources/AdResource.php
@@ -5,17 +5,17 @@ declare(strict_types=1);
 namespace App\MoonShine\Resources;
 
 use App\Enums\Gender;
-use MoonShine\Fields\Image;
-use Moonshine\Fields\Image as MoonshineImage;
+use App\Models\Images;
+use Faker\Core\Number;
+use Faker\Provider\Text;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Ad;
 
 use Illuminate\Support\Str;
 use MoonShine\Fields\Enum;
-use MoonShine\Fields\Number;
+use MoonShine\Fields\Image;
 use MoonShine\Fields\Relationships\BelongsTo;
 use MoonShine\Fields\Relationships\HasMany;
-use MoonShine\Fields\Text;
 use MoonShine\Fields\Textarea;
 use MoonShine\Resources\ModelResource;
 use MoonShine\Decorations\Block;
@@ -40,32 +40,32 @@ class AdResource extends ModelResource
         return [
             Block::make([
                 ID::make()->sortable(),
-                MoonshineImage::make('Image', 'image_field')
+                Image::make('Image', 'image_field')
                     ->disk('public')
                     ->dir('images')
                     ->allowedExtensions(['jpg', 'png', 'jpeg'])
-                    ->afterSave(function ($imageField, $item) {
+                    ->afterSave(function($imageField, $item) {
                         $uniqueName = Str::uuid() . '.' . $imageField->getExtension();
                         $path = 'images/' . $uniqueName;
 
                         $imageField->storeAs('public/images', $uniqueName);
 
-                        Image::create([
+                        Images::query()->create([
                             'image_path' => $path,
                             'ad_id' => $item->id,
                         ]);
                     }),
-                Text::make("title"),
-                Text::make("description")->hideOnIndex(),
+                \MoonShine\Fields\Text::make("title"),
+                \MoonShine\Fields\Text::make("description")->hideOnIndex(),
                 Textarea::make("address"),
-                Number::make("rooms")->sortable(),
-                Number::make("price")->sortable(),
+                \MoonShine\Fields\Number::make("rooms")->sortable(),
+                \MoonShine\Fields\Number::make("price")->sortable(),
                 Enum::make("gender")->attach(Gender::class)->sortable(),
-                BelongsTo::make(label: 'branch', relationName: 'branch', resource: new BranchResource()),
+                BelongsTo::make(label: 'branch',relationName: 'branch' , resource: new BranchResource()),
                 BelongsTo::make(label: 'status', resource: new StatusResource()),
                 BelongsTo::make(label: 'user', resource: new UserResource()),
-                BelongsTo::make(label: 'Mualif', relationName: 'owner', resource: new  UserResource()),
-                HasMany::make("images", relationName: "images", resource: new ImagesResource())->onlyLink(),
+                BelongsTo::make(label: 'Mualif',relationName: 'owner' ,resource: new  UserResource()),
+                HasMany::make("images",relationName: "images" ,resource: new ImagesResource())->onlyLink(),
 
 
             ]),
